6833edfcf1aeb61e92e0fd9a10efca4534e46d9a
Mayank Sharma <mayanksharmasav97@gmail.com>
2025-01-29 22:50:07 +0530

    Enhance WebSocket initialization with CORS support and emit random data every 3 seconds

--- socket.js
+++ socket.js
@@ -1,16 +1,30 @@
-
 const socketIo = require('socket.io');
 const socketAuth = require('./middlewares/socketAuth');
 let io;
 
-
 const initWebSocket = (server) => {
-  io = socketIo(server);
-  io.use(socketAuth)
+  io = socketIo(server, {
+    cors: {
+      origin: '*', // Allow all origins
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type'],
+      credentials: true
+    }
+  });
+  // io.use(socketAuth);
   io.on('connection', (socket) => {
-        console.log('socket connected', socket.id)
-        socket.emit('connected')
-    });
+    console.log('socket connected', socket.id);
+    socket.emit('connected');
+
+    // Emit random data every 10 seconds
+    setInterval(() => {
+      const randomData = {
+        id: Math.floor(Math.random() * 1000),
+        value: Math.random().toString(36).substring(7),
+      };
+      socket.emit('randomData', randomData);
+    }, 3000);
+  });
 };
 
 const getIoInstance = () => {
@@ -18,7 +32,6 @@ const getIoInstance = () => {
     throw new Error("Socket.io instance not initialized!");
   }
   return io;
-}
-
+};
 
 module.exports = { initWebSocket, getIoInstance };
